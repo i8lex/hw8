065017cb0c909180aec744acfe0256007476cdb1
i8lex <cybeerid@gmail.com>
2022-08-26 21:06:36 +0300

    done

---
+++ index.js
@@ -0,0 +1,40 @@
+const store = {
+
+    name: `Service mobile`,
+
+    address: {
+        country: `Ukraine`,
+        city: `Odessa`,
+        street: `Odariya`,
+        houseNumber: 3
+    },
+};
+
+store.clients = {};
+
+store.clients = {
+    'Nataliia Kulybina': {
+        quantity: +[],
+        bonuses: +[]
+    },
+
+    'Yuriy Kruchinin': {
+        quantity: +[],
+        bonuses: +[]
+    },
+
+    'Yevhen Vuzlov': {
+        quantity: +[],
+        bonuses: +[]
+    },
+
+    'Anatolii Zhykhariev': {
+        quantity: +[],
+        bonuses: +[]
+    },
+
+    'Kovalova Olha': {
+        quantity: +[],
+        bonuses: +[]
+    }
+}
